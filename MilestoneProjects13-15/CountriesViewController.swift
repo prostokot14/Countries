cc86d1d7bb08a23ddca2bb6007fe394fea0e2ae1
Anton Kashnikov <anton.kashnikoff@icloud.com>
2023-11-18 18:14:47 +0100

    Created table view controller

--- MilestoneProjects13-15/CountriesViewController.swift
+++ MilestoneProjects13-15/CountriesViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  CountriesViewController.swift
 //  MilestoneProjects13-15
 //
 //  Created by Антон Кашников on 18/11/2023.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class CountriesViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,4 +16,3 @@ class ViewController: UIViewController {
 
 
 }
-
