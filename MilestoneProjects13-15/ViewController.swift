09de7c0de92da1ecff68726abf22ac71b7ebeea7
Anton Kashnikov <anton.kashnikoff@icloud.com>
2023-11-18 18:11:04 +0100

    Initial Commit

---
+++ MilestoneProjects13-15/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MilestoneProjects13-15
+//
+//  Created by Антон Кашников on 18/11/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
