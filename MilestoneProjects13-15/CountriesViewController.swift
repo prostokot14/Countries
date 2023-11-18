994ec47976889e69c352a0480fd2ef5a769bf270
Anton Kashnikov <anton.kashnikoff@icloud.com>
2023-11-18 18:38:11 +0100

    Added main screen with hardcoded data

--- MilestoneProjects13-15/CountriesViewController.swift
+++ MilestoneProjects13-15/CountriesViewController.swift
@@ -8,11 +8,39 @@
 import UIKit
 
 final class CountriesViewController: UITableViewController {
+    
+    let countries = ["France", "Germany", "USA", "Russia"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        title = "Countries"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "countryCell")
     }
 
 
+}
+
+extension CountriesViewController {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        countries.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "countryCell", for: indexPath)
+        
+        cell.accessoryType = .disclosureIndicator
+        
+        if #available(iOS 14.0, *) {
+            var content = cell.defaultContentConfiguration()
+            content.text = countries[indexPath.row]
+            cell.contentConfiguration = content
+        } else {
+            cell.textLabel?.text = countries[indexPath.row]
+        }
+        
+        return cell
+    }
 }
